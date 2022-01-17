6d941d9312aa311fbe27c230971f4917be19de60
jihyun12 <kkjjhhbb@naver.com>
2022-01-17 21:44:24 +0900

    [feat]sticker 테스트

--- src/routes/start.jsx
+++ src/routes/start.jsx
@@ -1,7 +1,63 @@
-import React from "react";
+import { React, useState } from "react";
+import { Stage, Layer } from "react-konva";
+import Stickers from "./../components/common/stickers";
 
+const initialRectangles = [
+  {
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 100,
+    fill: "red",
+    id: "rect1",
+  },
+  {
+    x: 150,
+    y: 150,
+    width: 100,
+    height: 100,
+    fill: "green",
+    id: "rect2",
+  },
+];
 function Start() {
-  return <div>시작페이지임둥</div>;
+  const [rectangles, setRectangles] = useState(initialRectangles);
+  const [selectedId, selectShape] = useState(null);
+  const checkDeselect = (e) => {
+    // deselect when clicked on empty area
+    const clickedOnEmpty = e.target === e.target.getStage();
+    if (clickedOnEmpty) {
+      selectShape(null);
+    }
+  };
+  return (
+    <Stage
+      width="375"
+      height={window.innerHeight}
+      onMouseDown={checkDeselect}
+      onTouchStart={checkDeselect}
+    >
+      <Layer>
+        {rectangles.map((rect, i) => {
+          return (
+            <Stickers
+              key={i}
+              shapeProps={rect}
+              isSelected={rect.id === selectedId}
+              onSelect={() => {
+                selectShape(rect.id);
+              }}
+              onChange={(newAttrs) => {
+                const rects = rectangles.slice();
+                rects[i] = newAttrs;
+                setRectangles(rects);
+              }}
+            />
+          );
+        })}
+      </Layer>
+    </Stage>
+  );
 }
 
 export default Start;
