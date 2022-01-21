2b941d6689d9281b0309aa11e8054c232d38e91b
Kim Yeri <98sena@naver.com>
2022-01-21 19:18:12 +0900

    feat common button

--- src/routes/start.jsx
+++ src/routes/start.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Button from "../components/common/Button";
 
 function Start() {
-  return <div>시작페이지임둥</div>;
+  return (
+    <div>
+      <Button to="/test">시작하기</Button>
+    </div>
+  );
 }
 
 export default Start;
