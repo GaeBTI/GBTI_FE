5775c2168beb54ebc5d75e68251f598a0c1f03a3
Kim Yeri <98sena@naver.com>
2022-01-22 21:46:42 +0900

    feat Question Section Layout with Questions

--- src/routes/test.jsx
+++ src/routes/test.jsx
@@ -1,16 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import TestBackgroundImg from '../assets/images/testBackgroundImg.png';
 import TestBackgroundImgWithC from '../assets/images/testBackgroundImgWithC.png';
 import TestTopCharacter from '../assets/images/testTopCharacter.png';
+import {TESTS} from "../assets/texts/questions"
 
 //flex container
 const TestContainer=styled.div`
+  widht:100vw;
   height: 100vh;
   background-size : cover;
-  background-image:url(${TestBackgroundImgWithC});
+  background-image:url(${TestBackgroundImg});
   display: flex;
 	align-items: center;
+  justify-content:center;
 	flex-direction: column;
 `;
 // width:287px;
@@ -19,40 +22,56 @@ const TestContainer=styled.div`
 //   margin-top: 136px;
 //   width:287px;
 // `;
-const CharacterBox=styled.div`
-  width:164px;
-  
-  background-size:cover;
-  background-image:url(${TestTopCharacter});
-
+const CharacterImgBox=styled.img`
+  max-width: 100%;
+  display: block;
 `;
 const QuestionBox=styled.div`
   background-color:grey;
-  margin-top:136px;
+
+  margin-bottom:34px;
   width:287px;
   height:278px;
+
   display:flex;
-  align-items:center;
-  justify-content: center;
+	flex-direction: column;
+	align-items: center;
+
+  position:relative;
+`;
+const QuestionNumText=styled.div`
+  width:83px;
+  height:44px;
+  margin: 4px 11px 2px 193px;
+  
+  font-size:20px;
+  text-align:center;
+  line-height:44px;
+
+  position:absolute;
 `;
 const QuestionText=styled.div`
   width:254px;
   font-size:20px;
   text-align:center;
+  position:absolute;
+  top:50%;
+  transform:translate(0,-50%);
 `;
 const AnswerBox=styled.div`
   background-color:yellow;
-  margin-top:34px;
-  width:288px;
+  width:287px;
   height:285px;
 `;
 function Test() {
+  const[currentNum,setCurrentNum]=useState(0);
+
   return (
     <TestContainer>
+        <CharacterImgBox src={TestTopCharacter}/>
         <QuestionBox>
-          <QuestionText>
-          어느덧 다가온 시험 기간. 시험 범위가 너무 많아 학교에서 밤샘 공부를 하게 되었다. 지금 시간은 새벽 3시. 화장실을 가려고 나왔는데 복도가 너무 어둡다.
-          </QuestionText> 
+          <QuestionNumText>{TESTS[currentNum].id}/{TESTS.length}</QuestionNumText>
+          <QuestionText>{TESTS[currentNum].question}</QuestionText> 
         </QuestionBox>
         <AnswerBox>
 
