b7dc51c1f9a7c17d09bc49884c0407d97a4bebb2
jihyun12 <kkjjhhbb@naver.com>
2022-01-26 16:08:16 +0900

    [feat]결과 페이지 레이아웃

--- src/App.js
+++ src/App.js
@@ -6,32 +6,35 @@ import Result from "./routes/result";
 import Loading from "./routes/loading";
 
 function App() {
-  const [scores,setScores]= useState([
+  const [scores, setScores] = useState([
     {
-      type:"EnI",
-      score:0
+      type: "EnI",
+      score: 0,
     },
     {
-      type:"NnS",
-      score:0
+      type: "NnS",
+      score: 0,
     },
     {
-      type:"TnF",
-      score:0
+      type: "TnF",
+      score: 0,
     },
     {
-      type:"PnJ",
-      score:0
+      type: "PnJ",
+      score: 0,
     },
   ]); // EnI, NnS, TnF, PnJ
-  console.log('app',scores);
+  console.log("app", scores);
   return (
     <div className="App" style={{ width: 375, margin: "auto" }}>
       <Router>
         <Routes>
           <Route exact path="/" element={<Start />}></Route>
-          <Route path="/test" element={<Test setScores={setScores}/>}></Route>
-          <Route path="/loading" element={<Loading finalScores={scores} />}></Route>
+          <Route path="/test" element={<Test setScores={setScores} />}></Route>
+          <Route
+            path="/loading"
+            element={<Loading finalScores={scores} />}
+          ></Route>
           <Route path="/result" element={<Result />}></Route>
         </Routes>
       </Router>
