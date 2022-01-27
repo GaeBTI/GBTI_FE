994f5dcb7ffa93a93879787aa989372d911f3851
jihyun12 <kkjjhhbb@naver.com>
2022-01-27 19:43:35 +0900

    [feat]이미지 제외 결과 페이지

--- src/components/resultPage/testResult.jsx
+++ src/components/resultPage/testResult.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import {
   Worst,
   Best,
@@ -12,8 +13,25 @@ import {
   SmstrComnt,
 } from "../../icons";
 import styles from "./testResult.module.css";
+import { MBTIS } from "../../assets/texts/results";
 import { KeyBackTape } from "./../../icons";
-function TestResult() {
+
+function TestResult({ mbti }) {
+  const navigate = useNavigate();
+  const featureList = MBTIS[mbti].CharList.map((c) => (
+    <div className={styles.charac}>{"0  \u00A0 \u00A0  \u00A0" + c}</div>
+  ));
+  const mindList = MBTIS[mbti].Mind.map((m) => (
+    <div className={styles.mindWord}>{m}</div>
+  ));
+  const scoreList = MBTIS[mbti].Score.map((s) => (
+    <div className={styles.mindScore}>{s}</div>
+  ));
+
+  const handleRestart = () => {
+    navigate("/");
+  };
+
   return (
     <section className={styles.page}>
       <section className={styles.header}>개강한 나는...!</section>
@@ -30,9 +48,9 @@ function TestResult() {
           </div>
         </div>
         <div className={styles.keyword}>
-          <div className={styles.keySnt}>인싸 중에 아싸, 아싸 중에 인싸</div>
+          <div className={styles.keySnt}>{MBTIS[mbti].keySentence}</div>
           <div className={styles.keyBox}>
-            <div className={styles.keyName}>골든리트리버</div>
+            <div className={styles.keyName}>{MBTIS[mbti].keyWord}</div>
             <div className={styles.keyBack}>
               <KeyBackTape />
             </div>
@@ -41,31 +59,28 @@ function TestResult() {
       </section>
       <section className={styles.mind}>
         <div className={styles.mindLevel}>
-          <MindLevel />
+          <div>
+            <MindLevel />
+          </div>
+          <div className={styles.mindWordBox}>{mindList}</div>
+          <div className={styles.mindScoreBox}>{scoreList}</div>
         </div>
         <div className={styles.mindBox}></div>
       </section>
       <section className={styles.majorSection}>
         <Major></Major>
         <div className={styles.majorName}>
-          연극영화과, 광고홍보학과
+          {MBTIS[mbti].Major}
           <div className={styles.majorTape}>
             <MajorMasking />
           </div>
         </div>
       </section>
-      <section>
-        <CharList />
-      </section>
-      <section className={styles.smstrSection}>
-        <div className={styles.smstrMent}>
-          <SmstrComnt />
+      <section className={styles.charSection}>
+        <div className={styles.charBox}>
+          <CharList />
         </div>
-        <div className={styles.smstrText}>
-          <div>사람은 쉽게 변하지 않는다.</div>
-          <div>다시 태어나는게 빠를듯?</div>
-        </div>
-        {/* <div className={styles.smstrBox}></div> */}
+        <div className={styles.listBox}>{featureList}</div>
       </section>
       <section className={styles.teamSection}>
         <div className={styles.teamBox}>
@@ -73,24 +88,34 @@ function TestResult() {
             <Best />
             <div></div>
             <div className={styles.teamText}>
-              <div>아가리</div>
-              <div>파이터</div>
+              <div>{MBTIS[mbti].best.word1}</div>
+              <div>{MBTIS[mbti].best.word2}</div>
             </div>
           </div>
           <div className={styles.teamRight}>
             <Worst />
             <div></div>
             <div className={styles.teamText}>
-              <div>타고난</div>
-              <div>과대타입</div>
+              <div>{MBTIS[mbti].worst.word1}</div>
+              <div>{MBTIS[mbti].worst.word2}</div>
             </div>
           </div>
         </div>
       </section>
+      <section className={styles.smstrSection}>
+        <div className={styles.smstrMent}>
+          <SmstrComnt />
+        </div>
+        <div className={styles.smstrText}>
+          <div id={styles.snt1}>{MBTIS[mbti].promise.snt1}</div>
+          <div>{MBTIS[mbti].promise.snt2}</div>
+        </div>
+        {/* <div className={styles.smstrBox}></div> */}
+      </section>
       <section>
         <div className={styles.shareText}>테스트 공유하기</div>
       </section>
-      <section className={styles.retry}>
+      <section className={styles.retry} onClick={handleRestart}>
         <RetryTag />
       </section>
     </section>
