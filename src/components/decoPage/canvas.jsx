d2de59a0aa6c899837c2175d326dc7c366656caf
jihyun12 <kkjjhhbb@naver.com>
2022-02-21 15:05:52 +0900

    [fix]스티커 이미지 별 id => 각 스티커의 id로 변경

--- src/components/decoPage/canvas.jsx
+++ src/components/decoPage/canvas.jsx
@@ -3,39 +3,38 @@ import { Stage, Layer, Image } from "react-konva";
 import { useLinkClickHandler } from "react-router-dom";
 import useImage from "use-image";
 import Stickers from "../common/stickers";
-import {BackgroundImgList} from "./data.jsx"
-const canvasSize=335;
+import { BackgroundImgList } from "./data.jsx";
+const canvasSize = 335;
 
-const BackgroundImg=({bgImgCnt,name})=>{
+const BackgroundImg = ({ bgImgCnt, name }) => {
   console.log(BackgroundImgList[bgImgCnt]);
-  const [image]=useImage(require(`../../assets/images/background/${BackgroundImgList[bgImgCnt].url}`))
-  return <Image image={image} width={canvasSize} height={canvasSize} name={name}/>
-}
+  const [image] = useImage(
+    require(`../../assets/images/background/${BackgroundImgList[bgImgCnt].url}`)
+  );
+  return (
+    <Image image={image} width={canvasSize} height={canvasSize} name={name} />
+  );
+};
 
-function Canvas({dragUrl,images,setImages,bgImgCnt}) {
+function Canvas({ dragUrl, images, setImages, bgImgCnt }) {
   const [selectedId, selectShape] = useState(null);
-
-  const checkDeselect = (e) => { 
+  const checkDeselect = (e) => {
     // deselect when clicked on empty area
-    console.log(e.target.attrs.name,e.target.getStage());
-    const clickedOnEmpty = e.target.attrs.name==="background"; // e.target === e.target.getStage();
+    console.log(e.target.attrs.name, e.target.getStage());
+    const clickedOnEmpty = e.target.attrs.name === "background"; // e.target === e.target.getStage();
     if (clickedOnEmpty) {
       selectShape(null);
     }
-  }; 
+  };
   console.log("selectedId", selectedId);
   //const dragUrl = useRef();
   const stageRef = useRef();
   // const [images, setImages] = useState([]);
 
-  console.log(images);
-  console.log(dragUrl);
-  console.log("stage", stageRef.current);
-
   return (
     <div
       style={{
-        marginBottom:21
+        marginBottom: 21,
       }}
     >
       <div
@@ -66,18 +65,23 @@ function Canvas({dragUrl,images,setImages,bgImgCnt}) {
           onMouseDown={checkDeselect}
           onTouchStart={checkDeselect}
           ref={stageRef}
-          style={{backgroundImage:`url(require(../../assets/images/background/${BackgroundImgList[bgImgCnt].url}))`}}
+          style={{
+            backgroundImage: `url(require(../../assets/images/background/${BackgroundImgList[bgImgCnt].url}))`,
+          }}
         >
           <Layer>
-            <BackgroundImg bgImgCnt={bgImgCnt} name="background"/>
+            <BackgroundImg bgImgCnt={bgImgCnt} name="background" />
             {images.map((image, i) => {
               return (
                 <Stickers
+                  setImages={setImages}
+                  images={images}
                   key={i}
+                  selectedId={selectedId}
                   shapeProps={image}
-                  isSelected={image.id === selectedId}
+                  isSelected={i === selectedId}
                   onSelect={() => {
-                    selectShape(image.id);
+                    selectShape(i);
                   }}
                   onChange={(newAttrs) => {
                     const imgs = images.slice();
