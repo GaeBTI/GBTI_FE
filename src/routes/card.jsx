8b674fa9eaff1390e52da5953ef89961936548c5
Kim Yeri <98sena@naver.com>
2022-02-22 21:19:56 +0900

    [feat] card page에 안내 문구 추가

--- src/routes/card.jsx
+++ src/routes/card.jsx
@@ -5,6 +5,7 @@ import KeywordBox from "../components/common/keywordBox/keywordBox";
 import { MBTIS } from "../assets/texts/results";
 import StartDecoButton from "../components/cardPage/startDecoButton";
 import ResultCard from "../components/cardPage/resultCard";
+import styled from "styled-components";
 
 function Card({ mbti }) {
   return (
@@ -14,8 +15,24 @@ function Card({ mbti }) {
         <ResultCard mbti={mbti}></ResultCard>
       </section>
       <KeywordBox MBTIS={MBTIS} mbti={mbti}></KeywordBox>
+      <CheerSentence>나만의 카드를 꾸며보세요!</CheerSentence>
       <StartDecoButton></StartDecoButton>
     </Container>
   );
 }
 export default Card;
+
+const CheerSentence=styled.div`
+  width: 166px;
+  height: 25px;
+
+  font-family: IM_Hyemin-Regular;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 15px;
+  line-height: 25px;
+
+  text-align: center;
+
+  color: #727272;
+`;
