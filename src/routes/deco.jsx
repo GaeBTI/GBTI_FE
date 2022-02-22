31d2e0ab155bb2c76be74fb5f58c42af15773b1f
jihyun12 <kkjjhhbb@naver.com>
2022-02-22 14:18:08 +0900

    [feat]이미지export

--- src/routes/deco.jsx
+++ src/routes/deco.jsx
@@ -6,32 +6,50 @@ import Canvas from "./../components/decoPage/canvas";
 import ChooseBackgroundBox from "../components/decoPage/chooseBackgroundBox";
 import GoResultButton from "../components/decoPage/goResultButton";
 
-function Deco({mbti}) {
+function Deco({ mbti }) {
   //dragurl, images
   console.log(mbti);
   const [images, setImages] = useState([
     {
-        x: 170,
-        y: 200,
-        src: require(`../assets/images/characters/${mbti}.png`),
-        id: String(1), //images.length.toString(),
-        w: 200,
-        h: 200,
-      }
+      x: 170,
+      y: 200,
+      src: require(`../assets/images/characters/${mbti}.png`),
+      id: String(1), //images.length.toString(),
+      w: 200,
+      h: 200,
+    },
   ]);
+  const [decoDone, setDone] = useState(false);
   //const [dragUrl,setDragUrl] = useState('');
-  const dragUrl= useRef();
-  console.log("out !!",images);
-  const [bgImgCnt,setBgImgCnt]= useState(0);
+  const dragUrl = useRef();
+  console.log("out !!", images);
+  const [bgImgCnt, setBgImgCnt] = useState(0);
   //drag url ref
-  return(
+
+  const isDone = () => {
+    console.log("decodecodonedone");
+    setDone(true);
+  }; //데코 마무리 됐으면
+  return (
     <Container>
-      <Canvas dragUrl={dragUrl} images={images} setImages={setImages} bgImgCnt={bgImgCnt} mbti={mbti}></Canvas>
-      <ChooseBackgroundBox setBgImgCnt={setBgImgCnt} bgImgCnt={bgImgCnt}></ChooseBackgroundBox>
+      <Canvas
+        dragUrl={dragUrl}
+        images={images}
+        setImages={setImages}
+        bgImgCnt={bgImgCnt}
+        mbti={mbti}
+        decoDone={decoDone}
+      ></Canvas>
+      <ChooseBackgroundBox
+        setBgImgCnt={setBgImgCnt}
+        bgImgCnt={bgImgCnt}
+      ></ChooseBackgroundBox>
       <StickerBox dragUrl={dragUrl} setImages={setImages}></StickerBox>
-      <GoResultButton></GoResultButton>
+      <button onClick={isDone}>
+        <GoResultButton></GoResultButton>
+      </button>
     </Container>
-  ) 
+  );
 }
 
 export default Deco;
