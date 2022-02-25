453793b09400324e1cbd0dc1b9e001d6272ae4ad
Kim Yeri <98sena@naver.com>
2022-02-25 20:24:21 +0900

    [refactor] change container background image

--- src/components/common/Container.jsx
+++ src/components/common/Container.jsx
@@ -1,14 +1,19 @@
 import styled from "styled-components";
-import TestBackgroundImg from '../../assets/images/testBackgroundImg.svg';
+import backgroundImg from "../../assets/images/background.svg";
 
 const Container=styled.div`
   height:100vh;
   background-size : cover;
-  background-image:url(${TestBackgroundImg});
+  background-image:url(${backgroundImg});
 
   display: flex;
   align-items: center;
   justify-content:center;
   flex-direction: column;
 `;
+//background-image:url("/public/images/background.svg");
+// const Container=()=>{
+//   return <ContainerDiv></ContainerDiv>
+// }
+
 export default Container;
