4765c68ea0a201f1537a8093a9a53fee60da073f
Kim Yeri <98sena@naver.com>
2022-03-03 23:15:44 +0900

    [style] 모달 리스트 색 수정

--- src/components/resultPage/FullCharModal.jsx
+++ src/components/resultPage/FullCharModal.jsx
@@ -2,59 +2,7 @@ import { React, useEffect } from "react";
 import styled from "styled-components";
 import { MBTIS } from "../../assets/texts/results";
 import Modal from "../common/Modal";// components/common/Modal/index.jsx"
-const ContentBox=styled.div`
-    display:flex;
-    justify-content: center;
-    align-items: center;
-    flex-direction: column;
 
-    font-family: IM_Hyemin-Regular;
-    padding:10px;
-`;
-const TitleText=styled.div`
-    width: 167px;
-    height: 31px;
-    margin-top:28px;
-
-    font-style: normal;
-    font-weight: bold;
-    font-size: 22px;
-    line-height: 26px;
-
-    text-align: center;
-`;
-const MbtiCardBox=styled.div`
-    width: 305px;
-    height: 137px;
-    margin-top:17px;
-
-    background: #EDFFF9;
-
-    display:flex;
-    justify-content: center;
-    align-items: center;
-`
-const CharImg=styled.img`
-    width:120px;
-    height:120px;
-`;
-const CharKeyBox=styled.div`
-    width:180px;
-    height:70px;
-
-    font-weight: 900;
-`;
-const CharKeySentenceText=styled.div`
-    font-size: 15px;
-    line-height: 22px;
-
-    color: #585858;
-`;
-const CharKeyWordText=styled.div`
-    font-size: 20px;
-    line-height: 26px;
-    color: #3C3C3C;
-`;
 const FullCharModal=({setIsOpen})=>{
     console.log(Object.keys(MBTIS));
     // 모달 오버레이에서 스크롤 방지
@@ -93,5 +41,59 @@ const FullCharModal=({setIsOpen})=>{
         </ContentBox>
     </Modal>
 }
+const ContentBox=styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
 
+    font-family: IM_Hyemin-Regular;
+    padding:10px;
+`;
+const TitleText=styled.div`
+    width: 167px;
+    height: 31px;
+    margin-top:28px;
+
+    font-style: normal;
+    font-weight: bold;
+    font-size: 22px;
+    line-height: 26px;
+
+    text-align: center;
+`;
+const MbtiCardBox=styled.div`
+    width: 305px;
+    height: 137px;
+    margin-top:17px;
+
+    background: #FFFDF1;
+
+    display:flex;
+    justify-content: center;
+    align-items: center;
+`
+const CharImg=styled.img`
+    width:120px;
+    height:120px;
+    filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.5));
+`;
+const CharKeyBox=styled.div`
+    width:180px;
+    height:70px;
+
+    font-weight: 900;
+`;
+const CharKeySentenceText=styled.div`
+    font-size: 15px;
+    line-height: 22px;
+
+    color: #585858;
+`;
+const CharKeyWordText=styled.div`
+    font-size: 20px;
+    line-height: 26px;
+    color: #3C3C3C;
+    -webkit-text-stroke: 0.75px #3C3C3C;
+`;
 export default FullCharModal;
