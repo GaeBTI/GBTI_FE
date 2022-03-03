0ee07ef26dd9a329d274c4b612c0468914dce837
jihyun12 <kkjjhhbb@naver.com>
2022-03-03 15:08:14 +0900

    [feat]google analytics 페이지 별 확인 및 결과 공유하기 track

--- src/components/resultPage/share/TwitterShare.jsx
+++ src/components/resultPage/share/TwitterShare.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import { TwitterIcon } from "./../shareIcons";
+import ReactGA from "react-ga";
 function TwitterShare() {
   const url = window.location.href;
   function shareTwitter() {
     const sendText = "개강해지는 엠비탸이♥"; // 전달할 텍스트
     const sendUrl = url; // 전달할 URL
+    ReactGA.event({
+      category: "result",
+      action: `Twitter Share`,
+    });
     window.open(
       "https://twitter.com/intent/tweet?text=" + sendText + "&url=" + sendUrl
     );
