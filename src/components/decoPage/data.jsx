e61667c1527e62b253ca83153bca88de90d7ff36
Kim Yeri <98sena@naver.com>
2022-03-03 23:57:08 +0900

    [style] 배경 이름 지어주기

--- src/components/decoPage/data.jsx
+++ src/components/decoPage/data.jsx
@@ -1,6 +1,39 @@
-const BGImgLength= 9;
-let BackgroundImgList=[];
-for(var i =0;i<BGImgLength;i++){
-  BackgroundImgList.push({name:`배경 ${i}`,url:`bg${i}.png`});
-}
+const BackgroundImgList=[
+  {
+    name:'기본',
+    url:'bg0.png'
+  },
+  {
+    name:'윈도우 하늘',
+    url:'bg1.png'
+  },
+  {
+    name:'ZOOM',
+    url:'bg2.png'
+  },
+  {
+    name:'벚꽃',
+    url:'bg3.png'
+  },
+  {
+    name:'우주',
+    url:'bg4.png'
+  },
+  {
+    name:'파티룸',
+    url:'bg5.png'
+  },
+  {
+    name:'스프링클',
+    url:'bg6.png'
+  },
+  {
+    name:'강의실',
+    url:'bg7.png'
+  },
+  {
+    name:'무지개',
+    url:'bg8.png'
+  },
+];
 export { BackgroundImgList };
