d449c3791d28bb554d511cca04e59090aa799c0a
Kim Yeri <98sena@naver.com>
2022-09-17 17:25:02 +0900

    [feat] startPage theme 적용

--- src/components/startPage/share/share.jsx
+++ src/components/startPage/share/share.jsx
@@ -1,20 +1,42 @@
 import React from "react";
 import styles from "./share.module.css";
-import FacebookShare from "./../../resultPage/share/facebookShare";
 import CopyLink from "./../../resultPage/share/copyLink";
 import KakaoMainShare from "./../../resultPage/share/kakaoMainShare/kakaoMainShare";
 import TwitterShare from "./../../resultPage/share/TwitterShare";
+import styled from "styled-components";
+
+const ShareSection = styled.section`
+  margin-top: 116px;
+`;
+const ShareText = styled.div`
+  font-family: ${({ theme }) => theme.mainFont};
+  font-style: normal;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 26px;
+  text-align: center;
+
+  color: ${({ theme }) => theme.primary};
+`;
+const ShareBox = styled.div`
+  margin-block: 1rem;
+  width: 170px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-around;
+`;
 function Share() {
   return (
-    <section className={styles.shareSection}>
-      <div className={styles.shareText}>테스트 공유하기</div>
-      <div className={styles.shareBox}>
+    <ShareSection>
+      <ShareText>테스트 공유하기</ShareText>
+      <ShareBox>
         <KakaoMainShare></KakaoMainShare>
         <TwitterShare></TwitterShare>
         {/* <FacebookShare></FacebookShare> */}
         <CopyLink></CopyLink>
-      </div>
-    </section>
+      </ShareBox>
+    </ShareSection>
   );
 }
 
